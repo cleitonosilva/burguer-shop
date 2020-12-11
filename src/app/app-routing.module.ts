ae9e2db8d876c9d74f3b797a1c3aa27848e0a1ce
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-12-11 15:13:37 -0300

    Aplly router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DrinksComponent } from './pages/drinks/drinks.component';
 import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'bebidas', component: DrinksComponent}
 ];
 
 @NgModule({
