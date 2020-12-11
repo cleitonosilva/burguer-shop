655a67342523fa9fda9baab891edfabf8e70ce09
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-12-11 18:48:32 -0300

    :heavy_plus_sign: implement drink components

---
+++ src/app/components/drinks-list/drinks-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drinks-list',
+  templateUrl: './drinks-list.component.html',
+  styleUrls: ['./drinks-list.component.scss']
+})
+export class DrinksListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
