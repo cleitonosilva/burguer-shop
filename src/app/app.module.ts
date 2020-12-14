c3d71e64819d80c79d70b33285936b225c3b6cf1
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-12-14 11:48:20 -0300

    adding dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { PromotionsHomeComponent } from './components/promotions-home/promotions
 import { FooterComponent } from './components/footer/footer.component';
 import { DrinksComponent } from './pages/drinks/drinks.component';
 import { DrinksListComponent } from './components/drinks-list/drinks-list.component';
+import {LocalStorageModule} from 'angular-2-local-storage'
+import { CommonModule } from '@angular/common';
+import { GenericProductListComponent } from './shared/generic-product-list/generic-product-list.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +29,7 @@ import { DrinksListComponent } from './components/drinks-list/drinks-list.compon
     FooterComponent,
     DrinksComponent,
     DrinksListComponent,
+    GenericProductListComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,6 +39,8 @@ import { DrinksListComponent } from './components/drinks-list/drinks-list.compon
     // BsDropdownModule.forRoot(),
     // FormsModule,
     RouterModule,
+    LocalStorageModule.forRoot({storageType : 'localStorage'}),
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
