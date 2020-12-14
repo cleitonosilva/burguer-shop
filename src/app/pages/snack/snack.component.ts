3d64653dad1fa769a9d6a10c551f08cf22f7c4f4
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-12-14 14:40:57 -0300

    ⚡️ calling the filtered list of data

--- src/app/pages/snack/snack.component.ts
+++ src/app/pages/snack/snack.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import {products} from '../../data/products-list-source';
+import {Produto} from '../../models/products.model'
+
 @Component({
   selector: 'app-snack',
   templateUrl: './snack.component.html',
   styleUrls: ['./snack.component.scss']
 })
 export class SnackComponent implements OnInit {
+  produtos: Produto[];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    const filterProduct = products.filter(p =>  p.categoria == 'Lanches');
+    this.produtos = filterProduct
   }
 
 }
