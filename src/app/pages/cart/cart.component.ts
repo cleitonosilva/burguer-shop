fee7e1fd1a48520c11ee0390102872f4bfa3649e
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-14 16:33:41 -0300

    skeleton of the finished cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  valor: number = 1;
+  valorTotal: number; 
+  valorProduto: number = 8
+
   constructor() { }
 
   ngOnInit(): void {
+   this.valorTotal= (this.valorProduto * this.valor)
   }
+
+  decrement(){
+    if(this.valor > 0){
+    this.valor--;
+    const total = (this.valorProduto * this.valor) 
+    this.valorTotal = total
+  
+  }
+}
+
+  increment(){
+    this.valor++;
+    const total = (this.valorProduto * this.valor) 
+    this.valorTotal = total
+  }
+
 
 }
