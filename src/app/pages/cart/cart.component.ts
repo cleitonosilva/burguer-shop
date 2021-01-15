5072ff8c17c45aa34c2b905a89be8aa93b40c61b
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-15 20:43:19 -0300

    implementing cart features

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from 'src/app/models/products.model';
 import { ProdutosService } from 'src/app/services/produtos.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -9,50 +10,62 @@ import { ProdutosService } from 'src/app/services/produtos.service';
 })
 export class CartComponent implements OnInit {
 
-  quantidadeInicial: number = 0;
-  valorTotal: number = 0; 
-  valorProduto: number 
   produtos: Produto [] = []
   produtosVindoDaPagina: Produto []
-  produtosSoma : Produto
   preco: string
-
-  constructor(public produtosService: ProdutosService) { }
+  totalItens: number =1
+  itemQuantidade: number = 0
+  valorTotal: number = 0 
+  
+  constructor(public produtosService: ProdutosService, private router: Router) { }
   
   ngOnInit(): void {
-
+    
     const storageValue = JSON.parse(String(localStorage.getItem('cart')));
     this.produtosVindoDaPagina = storageValue;
     
     this.produtosVindoDaPagina = this.produtosVindoDaPagina.map( x => ({...x, quantidade: 1, valorTotal: x.preco}))
     
-    
-    this.valorTotal= (this.valorProduto * this.quantidadeInicial)
     this.produtosService.getProdutos().subscribe(x =>{ 
-    this.produtos = x
-  })
-  }
+      this.produtos = x
+    })
+    this.itemQuantidade = this.produtosVindoDaPagina.length
 
+    for(let i of this.produtosVindoDaPagina){
+      this.valorTotal += i.valorTotal 
+    }
+    
+  }
+  
   decrement(item : Produto){
     if(item.quantidade > 1 ){
-    this.produtosVindoDaPagina = this.produtosVindoDaPagina.map(
-      x => x.id == item.id ? ({...x, quantidade: x.quantidade - 1}) : x)
-    }
+      this.produtosVindoDaPagina = this.produtosVindoDaPagina.map(
+        x => x.id == item.id ? ({...x, quantidade: x.quantidade - 1}) : x)
+        this.itemQuantidade--
+        this.valorTotal -= item.preco  
+      }
     this.produtosVindoDaPagina = this.produtosVindoDaPagina.map( 
       x => x.id == item.id ? ({...x, valorTotal: x.preco * x.quantidade}) : x)
-  }
+    }
     
     increment(item: Produto){
-    
-    this.produtosVindoDaPagina = this.produtosVindoDaPagina.map(
-      x => x.id === item.id ? ({...x, quantidade: x.quantidade + 1}) : x)
-      this.produtosVindoDaPagina = this.produtosVindoDaPagina.map( 
-        x => x.id == item.id ? ({...x, valorTotal: x.preco * x.quantidade}) : x)
+      this.produtosVindoDaPagina = this.produtosVindoDaPagina.map(
+        x => x.id === item.id ? ({...x, quantidade: x.quantidade + 1}) : x)
+        this.itemQuantidade++
+
+        this.produtosVindoDaPagina = this.produtosVindoDaPagina.map( 
+          x => x.id == item.id ? ({...x, valorTotal: x.preco * x.quantidade}) : x)
+
+        // ----------------------------- A representa o valor inicial B representa o proximo valor da lista a ser somado com o passado que é o A 
+        this.valorTotal = this.produtosVindoDaPagina.reduce((a,b) => a + b.valorTotal, 0 )  
+        }
+        
+    finishCar(){
+      localStorage.clear();
+      this.router.navigate([''])
     }
 
 
-  pegarValor(valor : any){
-    
-  }
-
 }
+        
+      
